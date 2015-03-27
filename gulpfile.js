9f0d69349831a68ec61f6064dc101b1064ad1460
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-03-27 19:16:42 +0100

    optimized build process and added JS compression

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,49 @@ var livereload   = require('gulp-livereload');
 var minifyCSS    = require('gulp-minify-css');
 var rename       = require('gulp-rename');
 var sass         = require('gulp-ruby-sass');
-var gutil        = require('gulp-util');
+var size         = require('gulp-size');
+var uglify       = require('gulp-uglify');
 
 // Styles
 gulp.task('styles', function () {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src(['src/scss/**/*.scss'])
     .pipe(sass({ style: 'compact', precision: 7, sourcemap: true }))
-    .on('error', gutil.log)
     .pipe(autoprefixer('last 2 versions'))
-    .on('error', gutil.log)
+    .pipe(size({ title: 'CSS' }))
     .pipe(gulp.dest('dist/css'));
 });
 
-// Minify
-gulp.task('minify', ['styles'], function () {
+// Minify CSS
+gulp.task('minify_css', ['styles'], function () {
   return gulp.src(['dist/css/collectorium.css'])
     .pipe(minifyCSS())
     .pipe(cssshrink())
     .pipe(rename(function (path) {
       path.basename += '.min';
     }))
+    .pipe(size({ title: 'Minified CSS' }))
     .pipe(gulp.dest('dist/css'))
+    .pipe(livereload());
+});
+
+// Minify JS
+gulp.task('minify_js', function() {
+  gulp.src(['src/js/**/*.js'])
+    .pipe(size({ title: 'JS' }))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.basename += '.min';
+    }))
+    .pipe(size({ title: 'Minified JS' }))
+    .pipe(gulp.dest('dist/js'))
     .pipe(livereload());
 });
 
 // Watch
 gulp.task('watch', function () {
-  gulp.watch('src/**/*.scss', ['styles', 'minify']);
+  gulp.watch('src/scss/**/*.scss', ['styles', 'minify_css']);
+  gulp.watch('src/js/**/*.js', ['minify_js']);
 });
 
 // Default
